723ad456bf81047d64b99f939199e906ef7bde6d
Kristo Krikmann <kristo.krikmann@gmail.com>
2025-10-24 15:35:53 +0300

    Grid tiles can be active and have color

--- Får Får Få Får/Assets/Scipts/Grid/GridTile.cs
+++ Får Får Få Får/Assets/Scipts/Grid/GridTile.cs
@@ -3,8 +3,51 @@ using UnityEngine;
 public class GridTile : MonoBehaviour
 {
     public Vector2 GridPos;
+
+    public Color UnactiveColor;
+    public Color ActiveColor;
+    public Color HoverColor;
+
+    private bool active = false;
+    private bool isHovered = false;
+
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        ApplyColor();
+    }
+
+    private void ApplyColor()
+    {
+        if (sr == null) return;
+        if (isHovered) sr.color = HoverColor;
+        else sr.color = active ? ActiveColor : UnactiveColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        isHovered = true;
+        ApplyColor();
+    }
+
+    private void OnMouseExit()
+    {
+        isHovered = false;
+        ApplyColor();
+    }
+
     private void OnMouseDown()
     {
-        GetComponent<SpriteRenderer>().color = Color.gray;
+        active = !active;
+        ApplyColor();
+    }
+
+    // optional helper if you want to set from other scripts
+    public void SetActive(bool value)
+    {
+        active = value;
+        ApplyColor();
     }
 }
