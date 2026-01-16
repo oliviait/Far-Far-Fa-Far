3279ce4b62e76076b18e67ef698a7814c88faa75
Kristo Krikmann <kristo.krikmann@gmail.com>
2026-01-16 21:21:53 +0200

    Empty inv preventing animation and sound

--- Får Får Få Får/Assets/Scripts/Farm/HUD.cs
+++ Får Får Få Får/Assets/Scripts/Farm/HUD.cs
@@ -1,21 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    public Button MapButton;
-    public Button BreedButton;
+    public Button mapButton;
+    public Button breedButton;
 
-    public AudioClipGroup MapCrunch;
+    public List<GameObject> inventorySlots;
+    public GameObject inventorySlotsHolder;
+    public AudioClipGroup mapCrunch;
+    public AudioClipGroup wrongAction;
 
-    public void onMapButtonClicked()
+    public void OnMapButtonClicked()
     {
-        MapCrunch.Play();
-        SceneManager.LoadScene(2);
+        if (Player.Instance.InventorySheepList.Count == 0)
+        {
+            Animator slotAnimator = inventorySlotsHolder.GetComponent<Animator>();
+            slotAnimator.SetTrigger("Wiggle");
+            foreach (var slot in inventorySlots)
+            {
+                slotAnimator = slot.GetComponent<Animator>();
+                slotAnimator.SetTrigger("Flash");
+            }
+
+            wrongAction.Play();
+        }
+        else
+        {
+            mapCrunch.Play();
+            SceneManager.LoadScene(2);
+        }
     }
 
-    public void onBreedButtonClicked()
+    public void OnBreedButtonClicked()
     {
         Breeding.Instance.Breed();
     }
